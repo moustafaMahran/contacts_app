a15ead08517ea93b49a09c6e39573bf371a47180
moustafaMahran <moustafa.mahran208@gmail.com>
2021-11-20 13:08:23 +0200

    adding schema file

--- backend/db/migrate/20211118165218_create_contacts.rb
+++ backend/db/migrate/20211118165218_create_contacts.rb
@@ -6,15 +6,15 @@ class CreateContacts < ActiveRecord::Migration[5.0]
     create_table :contacts do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
-      t.string :email, null: false, index: { unique: true }
+      t.string :email, null: false
       t.string :phone_number, null: false
 
       t.timestamps
     end
 
-    add_index :contacts, [:first_name, :last_name], :name => 'contact_name_index'
-    add_index :contacts, :email, :name => 'contact_email_index'
-    add_index :contacts, :phone_number, :name => 'contact_phone_number_index'
+    add_index :contacts, [:first_name, :last_name], :name => 'index_contacts_on_names'
+    add_index :contacts, :email, :name => 'index_contacts_on_email'
+    add_index :contacts, :phone_number, :name => 'index_contacts_on_phone_number'
 
   end
 end
---
+++ backend/db/schema.rb
@@ -0,0 +1,49 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20211118220401) do
+
+  create_table "audits", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "auditable_id"
+    t.string   "auditable_type"
+    t.integer  "associated_id"
+    t.string   "associated_type"
+    t.integer  "user_id"
+    t.string   "user_type"
+    t.string   "username"
+    t.string   "action"
+    t.text     "audited_changes", limit: 65535
+    t.integer  "version",                       default: 0
+    t.string   "comment"
+    t.string   "remote_address"
+    t.string   "request_uuid"
+    t.datetime "created_at"
+    t.index ["associated_type", "associated_id"], name: "associated_index", using: :btree
+    t.index ["auditable_type", "auditable_id", "version"], name: "auditable_index", using: :btree
+    t.index ["created_at"], name: "index_audits_on_created_at", using: :btree
+    t.index ["request_uuid"], name: "index_audits_on_request_uuid", using: :btree
+    t.index ["user_id", "user_type"], name: "user_index", using: :btree
+  end
+
+  create_table "contacts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "first_name",   null: false
+    t.string   "last_name",    null: false
+    t.string   "email",        null: false
+    t.string   "phone_number", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["email"], name: "index_contacts_on_email", using: :btree
+    t.index ["first_name", "last_name"], name: "index_contacts_on_names", using: :btree
+    t.index ["phone_number"], name: "index_contacts_on_phone_number", using: :btree
+  end
+
+end
