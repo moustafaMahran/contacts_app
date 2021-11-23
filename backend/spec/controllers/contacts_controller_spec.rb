083013cc201987040f0909a16ab30f34fe7a562c
moustafaMahran <moustafa.mahran208@gmail.com>
2021-11-23 15:43:15 +0200

    adding rspec files

---
+++ backend/spec/controllers/contacts_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe ContactsController, type: :controller do
+
+
+  # index action
+  describe 'GET #index' do
+    before do
+      get :index
+    end
+
+    it 'is expected to assign user instance variable' do
+      expect(response.status).to eq(200)
+    end
+  end
+  # TODO the rest of endpoints
+end
---
+++ backend/spec/models/contact_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.describe Contact, type: :model do
+
+  let(:params) { {} }
+
+  describe 'model validations' do
+    context 'when all attributes exist' do
+      before do
+        params = {first_name: 'patrick', last_name: 'jane', phone_number: '+201091765367', email: 'jane@test.com'}
+        @contact = Contact.create(params)
+      end
+      it 'should be valid' do
+        expect(@contact).to be_valid
+      end
+    end
+
+    context 'when first_name is missing' do
+      before do
+        params = {last_name: 'jane', phone_number: '+201091765367', email: 'jane@test.com'}
+        @contact = Contact.create(params)      
+      end
+      it 'should be invalid' do
+        expect(@contact.valid?).to eq(false) 
+      end
+    end
+
+    context 'when last_name is missing' do
+      before do
+        params = {first_name: 'jane', phone_number: '+201091765367', email: 'jane@test.com'}
+        @contact = Contact.create(params)      
+      end
+      it 'should be invalid' do
+        expect(@contact.valid?).to eq(false) 
+      end
+    end
+
+    context 'when phone_number is missing' do
+      before do
+        params = {first_name: 'patrick', last_name: 'jane', email: 'jane@test.com'}
+        @contact = Contact.create(params)      
+      end
+      it 'should be invalid' do
+        expect(@contact.valid?).to eq(false) 
+      end
+    end
+
+    context 'when email is missing' do
+      before do
+        params = {first_name: 'patrick', last_name: 'jane', phone_number: '+201091765367'}
+        @contact = Contact.create(params)      
+      end
+      it 'should be invalid' do
+        expect(@contact.valid?).to eq(false) 
+      end
+    end
+
+    context 'when email is taken' do
+      before do
+        params1 = {first_name: 'patrick', last_name: 'jane', phone_number: '+201091765367', email: 'jane@test.com'}
+        params2 = {first_name: 'patrick', last_name: 'jane', phone_number: '+201091765367', email: 'jane@test.com'}
+        @contact1 = Contact.create(params1)      
+        @contact2 = Contact.create(params2)      
+      end
+      it 'should be invalid' do
+        expect(@contact1.valid?).to eq(true)
+        expect(@contact2.valid?).to eq(false)
+      end
+    end
+  end
+end
