4b199b03540576c7cc238dd10e91c6833c504c06
moustafaMahran <moustafa.mahran208@gmail.com>
2022-12-18 02:43:35 +0200

    adding audit web layer

---
+++ backend/app/layers/web/controllers/audits_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+module Web
+  module Controllers
+    class AuditsController < ApplicationController
+      def show
+        command = Application::Audit::Commands::ShowAuditHistoryCommand.new(
+          auditable_id: permitted_params[:id],
+          auditable_type: permitted_params[:auditable_type]
+        )
+        audits = Application::Audit::AuditApplication.new.show_edit_history(command)
+        render json: audits, status: :ok, each_serializer: Serializers::AuditSerializer
+      end
+
+      private
+
+      def permitted_params
+        params.permit(:auditable_type, :id)
+      end
+
+      def render_missing_params
+        render json: { error: 'missing required params' }, status: :bad_request
+      end
+    end
+  end
+end
